009137adff919dfcaf0db6f8efe17cce4ac05d8b
Jonn Tan <jonntan@Jonns-MacBook-Pro.local>
2021-01-04 17:08:59 +0800

    Set up commands for config

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd opt/islandFurniture_FYP/
+npm init
+npm install mysql
+npm install body-parser
+npm install express
+npm install request
+npm install nodemailer
+npm install bcrypt
+npm install stripe
+npm install @fortawesome/fontawesome-free
+npm install pretty-checkbox
+npm install jsonwebtoken
+npm install multer
+npm install forever -g
+forever server.js
